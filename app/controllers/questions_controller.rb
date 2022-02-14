ae0f05206143b8b172cec25e8fa75387a6681dfc
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-02-14 14:26:40 +0000

    it works

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,17 @@
+
 class QuestionsController < ApplicationController
+
   def ask
   end
 
   def answer
+    @question = params[:question]
+    if 'I am going to work'.downcase == @question.downcase
+      @stupid_answer = 'Great!'
+    elsif @question.ends_with?('?')
+      @stupid_answer = 'Silly question, get dressed and go to work!'
+    else
+      @stupid_answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
